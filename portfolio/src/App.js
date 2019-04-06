8cd051f87792bcba724592891eba662cfe349105
Sean ODonnell <odonnellsean9045@gmail.com>
2019-04-06 18:04:10 -0400

    set up routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/Main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
-    <Layout>
-        <Header title="Title" scroll>
-            <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-            </Navigation>
-        </Header>
-        <Drawer title="Title">
+        <Layout>
+            <Header title="Sean O'Donnell" scroll>
+                <Navigation>
+                <Link to="/Resume">Resume</Link>
+                <Link to="/AboutMe">About Me</Link>
+                <Link to="/Projects">Projects</Link>
+                <Link to="/Contact">Contact</Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
             <Navigation>
                 <a href="/">Link</a>
                 <a href="/">Link</a>
@@ -25,6 +27,7 @@ class App extends Component {
         </Drawer>
         <Content>
             <div className="page-content" />
+            <Main/>
         </Content>
     </Layout>
 </div>
